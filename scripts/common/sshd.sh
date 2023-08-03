c0a58d6eb484c3817d285bbb94d6c7c46f202203
huyuhao <huyuhao@kingsoft.com>
2023-08-03 19:52:26 +0800

    增加centos-8-aarch64镜像制作

--- scripts/common/sshd.sh
+++ scripts/common/sshd.sh
@@ -13,7 +13,7 @@ checkDirective() {
 
 checkDirective UseDNS no
 checkDirective PermitRootLogin yes
-checkDirective PasswordAuthentication yes
+#checkDirective PasswordAuthentication yes
 checkDirective GSSAPIAuthentication no
 checkDirective KbdInteractiveAuthentication no
 checkDirective ChallengeResponseAuthentication no
