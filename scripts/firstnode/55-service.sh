806b5ade28cfb1367d986094fa17265aa22fce1d
huyuhao <huyuhao@kingsoft.com>
2023-08-09 11:05:09 +0800

    修改制作centos7 ARM系统-修改repo

--- scripts/firstnode/55-service.sh
+++ scripts/firstnode/55-service.sh
@@ -56,6 +56,26 @@ EOF
 }
 
 function node-init() {
+    cat > /node/init.sh << EOF
+#!/bin/bash
+set -eux
+set -o pipefail
+
+export LC_ALL=C.UTF-8
+
+DATA_PATH=/data/packages
+
+if [[ ! -d "$DATA_PATH" ]]
+then
+    mkdir -p "$DATA_PATH"
+fi
+
+if ! mountpoint -q "$DATA_PATH"
+then
+    mount /dev/disk/by-label/penglai "\$DATA_PATH"
+fi
+bash "\$DATA_PATH"/houyi/packages/youchao-bootstrap/node-init.sh
+EOF
     chmod 755 /node-init.sh
     cat >/usr/lib/systemd/system/node-init.service <<EOF
 [Unit]
