ec967d27f7f277b23e73e04945df410db9f7c384
huyuhao <huyuhao@kingsoft.com>
2023-08-28 18:54:08 +0800

    制作aarch64-centos8

--- bin/build_image.sh
+++ bin/build_image.sh
@@ -27,11 +27,14 @@ done
 
 [ -z "$TEMPLATE" ] && echo "Error: Template file not set" && exit 1
 
-DIR_NAME="$(basename -s .json "$TEMPLATE")"
-TEMPLATE_NAME="$(basename -s .json "$TEMPLATE")"
+
 if [[ $TEMPLATE_NAME == *"hcl"* ]]; then
+  DIR_NAME="$(basename -s .json.pkr.hcl "$TEMPLATE")"
+  TEMPLATE_NAME="$(basename -s .json.pkr.hcl "$TEMPLATE")"
   IMAGE_NAME=$(grep vm_name "$TEMPLATE" | awk '{print $3}' | sed -e 's/\"//g' | sed -e 's/,//g')
 else
+  DIR_NAME="$(basename -s .json "$TEMPLATE")"
+  TEMPLATE_NAME="$(basename -s .json "$TEMPLATE")"
   IMAGE_NAME=$(grep vm_name "$TEMPLATE" | awk '{print $2}' | sed -e 's/\"//g' | sed -e 's/,//g')
 fi
 FINAL_QCOW_FILE_NAME="${DIR_NAME}/${IMAGE_NAME}-compressed.qcow2"
